5acf453b916071984bbe31eca5615590ce293bda
Ashish K <141225901+ash7724@users.noreply.github.com>
2025-04-20 20:04:25 +0530

    Update example_5.c

--- day5/example_5.c
+++ day5/example_5.c
@@ -4,37 +4,71 @@
 int main() 
 { 
   int var1,var2,sum;
-  char ch;
+  char ch,choice;
   int temp,rem,quo;
   float reml;
-
-  //5. C Program to Find ASCII Value of a Character
-  printf("\n Enter a caharacter  ");
-  scanf("%c",&ch);
-  //printf("\n Entered character is %c  ",ch);
-  printf("\n ASCII value of  character is %d  ",ch);
-
-  //6. Python Program to Swap Two Variables
-  printf("\n Enter a number  ");
-  scanf("%d",&var1);
-  printf("\n Enter second number  ");
-  scanf("%d",&var2);
-  temp=var1;
-  var1=var2;
-  var2=temp;
-  printf("\n Swapped numbers %d,%d  ",var1,var2);
-
-  //7. C Program to Compute Quotient and Remainder
-  printf("\n Taking these two numbers fr division %d/%d  ",var1,var2);
-  rem=var1%var2;
-  quo=var1/var2;
-  printf("\n Reaminder=%d and Qutient= %d for  %d/%d ",rem,quo,var1,var2);
-
-  //8. C Program to Find the Size of int, float, double and char]
-  printf("\n Size of Integer=%d ",sizeof(int));
-  printf("\n Size of float=%d ",sizeof(float));
-  printf("\n Size of double=%d ",sizeof(double));
-  printf("\n Size of char=%d ",sizeof(char));
   
-  // C Program to Demonstrate the Working of Keyword long  
+  printf("\n5.  C Program to Find ASCII Value of a Character");
+  printf("\n6.  C Program to Find the Size of int, float, double and char");
+  printf("\n7.  C Program to Compute Quotient and Remainder");
+  printf("\n8.  C Program to Demonstrate the Working of Keyword long");
+  printf("\n9.  C Program to Swap Two Variables");
+  printf("\nA. C Program to Check Whether a Number is Even or Odd ");
+  printf("\n Enter you Choice");
+  choice=getchar();
+  switch(choice) {
+    case '5':   
+        //5. C Program to Find ASCII Value of a Character
+        printf("\n Enter a caharacter  ");
+        scanf("%c",&ch);
+        //printf("\n Entered character is %c  ",ch);
+        printf("\n ASCII value of  character is %d  ",ch);
+        break;
+    case '6':
+        //6. C Program to Find the Size of int, float, double and char
+        printf("\n Size of Integer=%d ",sizeof(int));
+        printf("\n Size of float=%d ",sizeof(float));
+        printf("\n Size of double=%d ",sizeof(double));
+        printf("\n Size of char=%d ",sizeof(char));
+        break;
+
+    case '7':
+        //7. C Program to Compute Quotient and Remainder
+        printf("\n Taking these two numbers fr division %d/%d  ",var1,var2);
+        rem=var1%var2;
+        quo=var1/var2;
+        printf("\n Reaminder=%d and Qutient= %d for  %d/%d ",rem,quo,var1,var2);
+        break;
+      //8. Program to Demonstrate the Working of Keyword long
+    case '9': 
+        //9. C Program to Swap Two Variables
+        printf("\n Enter a number  ");
+        scanf("%d",&var1);
+        printf("\n Enter second number  ");
+        scanf("%d",&var2);
+        temp=var1;
+        var1=var2;
+        var2=temp;
+        printf("\n Swapped numbers %d,%d  ",var1,var2);
+    
+    case 'A': 
+        printf("\n Enter a number  ");
+        scanf("%d",&var1);
+        if(var1%2==0)
+            printf("\n Number %d is Even",var1);
+        else
+            printf("\n Number %d is odd",var1);
+            
+  }
+  printf("\n Enter you Choice");
+  choice=getchar();
+
+
+ 
+
+
+
+
+  
+  //10. C Program to Check Whether a Number is Even or Odd 
 }

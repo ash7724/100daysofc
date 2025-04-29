48778743b6577474d543df040500a5489b32a631
Ashish K <141225901+ash7724@users.noreply.github.com>
2025-04-29 22:24:30 +0530

    Create day1_sum_of_array.c

---
+++ day1_sum_of_array.c
@@ -0,0 +1,23 @@
+//C Program to Calculate Average Using Arrays
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Write C code her
+    int arr[15];
+    //int *array= malloc ( num * sizeof(int));
+    int sum=0;
+    int num;
+    printf("\n Enter number of elements to array");
+    scanf(" %d", &num);
+    
+    for (int i=0;i<num;i++){
+        printf("\n Enter %dth  Elements of the array  ",i+1);
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+
+    printf("\n Sum of  Elements of the array %d ",sum);
+
+    return 0;
+}
